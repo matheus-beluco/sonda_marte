ebcb6c49a47c69865b41e831c535aefcfee85058
Matheus Beluco <matheus@kenzie.com.br>
2021-11-16 21:03:04 -0300

    first version, only functional

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
 def probe_movement(x, y, direction, command):
-    ...
+    direction_values = {"N": 1, "S": -1, "E": 1, "W": -1}
+    direction_l = {"N": "W", "S": "E", "E": "N", "W": "S"}
+    direction_r = {"E": "S", "S": "W", "W": "N", "N": "E"}
+    for letter in command:
+        if letter == "L":
+            direction = direction_l[direction]
+            continue
+        if letter == "R":
+            direction = direction_r[direction]
+            continue
+        if letter == "M":
+            if direction == "E" or direction == "W":
+                x += direction_values[direction]
+            if direction == "N" or direction == "S":
+                y += direction_values[direction]
+    return f"{x} {y} {direction}"
